dcde06394e5f4f2ed552fb2ea3931a1bf2faf50c
HUSH365 <allidammylola365@gmail.com>
2024-08-21 10:44:34 +0100

    o

---
+++ event.js
@@ -0,0 +1,64 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('fa-times')
+  navbar.classList.toggle('active')
+}
+
+let themeToggler = document.querySelector('.theme-toggler');
+let toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.onclick = () => {
+  themeToggler.classList.toggle('active');
+}
+window.scroll = () => {
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  themeToggler.classList.remove('active');
+}
+document.querySelectorAll('.theme-toggler .theme-btn').forEach(btn => {
+  btn.onclick = () => {
+    let color = btn.style.background;
+    document.querySelector(':root').style.setProperty('--main-color', color);
+  }
+});
+var swiper = new Swiper(".home-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 3,
+    stretch: 0,
+    depth: 50,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+});
+var swiper = new Swiper(".review-slider", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1050: {
+      slidesPerView: 2.5,
+    },
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
+  },
+});
